9977c8d785e232ed888e31534685344441a7cd7f
NiteshStudaent <niteshstudent33@gmail.com>
2024-03-09 00:44:36 +0530

    firts prog1

---
+++ simpleAddition.py
@@ -0,0 +1,8 @@
+def addition(num1, num2):
+    return str (int (num1) + int(num2))
+    
+num1 = input ("Enter 1st number : ")
+num2 = input ("Enter 2nd number : ")
+
+print ("Addtion is : ", addition(num1, num2))
+
