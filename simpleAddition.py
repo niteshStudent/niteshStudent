4f6b855dee979c03d0ade338e1d34e4be8df5d17
nitseshstudent <niteshstudent33@gmail.com>
2024-03-09 01:03:48 +0530

    3rd num add

--- simpleAddition.py
+++ simpleAddition.py
@@ -1,8 +1,9 @@
-def addition(num1, num2):
-    return str (int (num1) + int(num2))
+def addition(num1, num2, num3):
+    return str (int (num1) + int(num2), int(num3))
     
 num1 = input ("Enter 1st number : ")
 num2 = input ("Enter 2nd number : ")
+num2 = input ("Enter 3rd number : ")
 
-print ("Addtion is : ", addition(num1, num2))
+print ("Addtion is : ", addition(num1, num2, num3))
 
