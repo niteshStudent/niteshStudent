689f7bf4a7f9d2ac0b0a106fbcbc6357ec37d933
niteshstudent <niteshstudent33@gmail.com>
2024-03-09 14:55:43 +0530

    add sub

---
+++ simplesub.py
@@ -0,0 +1,8 @@
+def sub (num1, num2):
+    return str (int (num1) - int(num2))
+
+num1 = input ("Enter 1st number : ")
+num2 = input ("Enter 2nd number : ")
+#num2 = input ("Enter 3rd number : ")
+
+print ("substraction is : ", sub(num1, num2,))
