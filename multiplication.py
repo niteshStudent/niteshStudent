f428697220289a93baf9dc7dfc5dccbb5676ad51
niteshstudent <niteshstudent33@gmail.com>
2024-03-09 15:41:45 +0530

    add multiple

---
+++ multiplication.py
@@ -0,0 +1,8 @@
+def multipliation(num1, num2):
+    return str (int (num1) * int(num2))
+
+num1 = input ("Enter 1st number : ")
+num2 = input ("Enter 2nd number : ")
+
+
+print ("multipliation is : ", multipliation(num1, num2))
